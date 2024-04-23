63a1c39d8d6e267bc411ee92550fe440241a3b62
pratik-0100 <pratik.shiradkar@digismartassets.com>
2024-04-24 00:32:44 +0530

    fix logout functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -165,8 +165,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToke: undefined,
+      $unset: {
+        refreshToke: 1,
       },
     },
     {
